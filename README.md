06af96dd50b3ba5e02c5b4359ac1b70511dcc389
unknown <tobasanmi@gmail.com>
2024-06-17 20:46:25 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # AWS
+
+Created a VPC (10.0.0.0/16)
+created 2 subnets that brought a local route table by default (10.0.0.0/24, 10.0.1.0/24)
+This above subnets are private due to local route table 
+Create internet gateway to allow inbound and outbound traffic to the VPC and the internet.
+Attach the internet gateway to the VPC
+Create a public route table 
+Associate the internet gateway to the public route table 
+Associate the public subnet to the public route table 
+on the VPC enable dns host name 
+on the public subnet enable auto assign IPV4 ips as public
--- main.tf
+++ main.tf
@@ -26,26 +26,77 @@ resource "aws_subnet" "my_subnet" {
   }
 }
 
-resource "aws_network_interface" "foo" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+# resource "aws_network_interface" "n_Interface1" {
+#   subnet_id   = aws_subnet.my_subnet.id
+#   private_ips = ["172.16.10.100"]
+
+#   tags = {
+#     Name = "primary_network_interface"
+#   }
+# }
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls_ssh"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "primary_network_interface"
+    Name = "allow_tls"
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.my_vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4        = aws_vpc.my_vpc.cidr_block
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+# resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv6         = aws_vpc.my_vpc.ipv6_cidr_block
+#   from_port         = 443
+#   ip_protocol       = "tcp"
+#   to_port           = 443
+# }
+# resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ipv6" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv6         = aws_vpc.my_vpc.ipv6_cidr_block
+#   from_port         = 22
+#   ip_protocol       = "ssh"
+#   to_port           = 22
+# }
+
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
+
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
+#   security_group_id = aws_security_group.allow_tls.id
+#   cidr_ipv6         = "::/0"
+#   ip_protocol       = "-1" # semantically equivalent to all ports
+# }
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = file("~/.ssh/id_rsa.pub") # Path to your public key file
 }
 
-resource "aws_instance" "foo" {
+resource "aws_instance" "testVM" {
   ami           = "ami-0776c814353b4814d" # us-west-2
   instance_type = "t2.micro"
 
   network_interface {
-    network_interface_id = aws_network_interface.foo.id
+    network_interface_id = aws_network_interface.n_Interface1.id
     device_index         = 0
   }
 
